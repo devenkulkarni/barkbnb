432743fae023f27a89b7261f8f533fca8d5a0ba3
Devendra Kulkarni <devendra.kulkarni@suse.com>
2026-07-16 16:12:01 +0530

    added PostgreSQL and GORM integration

--- backend/internal/app/app.go
+++ backend/internal/app/app.go
@@ -4,32 +4,50 @@ import (
 	"log/slog"
 
 	"github.com/devenkulkarni/barkbnb/backend/internal/config"
+	"github.com/devenkulkarni/barkbnb/backend/internal/database"
 	"github.com/devenkulkarni/barkbnb/backend/internal/logger"
 	"github.com/devenkulkarni/barkbnb/backend/internal/middleware"
 	"github.com/devenkulkarni/barkbnb/backend/internal/routes"
 	"github.com/gofiber/fiber/v3"
+	"gorm.io/gorm"
 )
 
 type Application struct {
-	Config *config.Config
-	Logger *slog.Logger
-	Server *fiber.App
+	Config   *config.Config
+	Logger   *slog.Logger
+	Server   *fiber.App
+	Database *gorm.DB
 }
 
-func NewApplication(cfg *config.Config) *Application {
+func NewApplication(cfg *config.Config) (*Application, error) {
 	appLogger := logger.Loggerfunc(cfg)
 	fiberApp := fiber.New()
 	fiberApp.Use(
 		middleware.RequestLogger(appLogger),
 	)
 
+	dbApp, err := database.NewDB(cfg)
+	if err != nil {
+		appLogger.Error("Failed to connect to the database", "error", err)
+
+		return nil, err
+	}
+
+	appLogger.Info(
+		"Connected to PostgreSQL",
+		"host", cfg.Database.Host,
+		"port", cfg.Database.Port,
+		"database", cfg.Database.Name,
+	)
+
 	routes.Register(fiberApp)
 
 	return &Application{
-		Config: cfg,
-		Server: fiberApp,
-		Logger: appLogger,
-	}
+		Config:   cfg,
+		Server:   fiberApp,
+		Logger:   appLogger,
+		Database: dbApp,
+	}, nil
 }
 
 func (a *Application) Run() error {
